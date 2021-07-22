1fca18736cb45b9bee692f3542ef6ef2266397da
Intel <86988525+Vakulen-co@users.noreply.github.com>
2021-07-22 14:04:05 +0300

    cryptogenius-pt

---
+++ cryptogenius-pt/config.php
@@ -0,0 +1,12 @@
+<?php
+$campaign_identifier = 'CAMPAIGN_KEY';
+$affiliate_hash = 'AFFILIATE_HASH';
+$landing_name = 'cryptogenius-pt';
+$language = 'PT';
+$base = '/cryptogenius-pt/';                // SLASHES ON SIDES
+$thankyou = 'thankyou.php'; // NO SLASHES AT START OF A STRING
+$FacebookPixel = isset($_GET['pixel_id']) ? $_GET['pixel_id'] : false;
+$TiktokPixel = isset($_GET['tp']) ? $_GET['tp'] : false;
+$GooglePixel = isset($_GET['gp']) ? $_GET['gp'] : false;
+$brokerRedirect = true;
+$hasPixel = false;          // IF WE USE ANY PIXEL EXCEPT FACEBOOK
---
+++ cryptogenius-pt/index.php
@@ -0,0 +1,243 @@
+<?php 
+include_once 'config.php'; 
+?>
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <base href="<?php echo $base ?>">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>The Crypto Genius</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="assets/css/css2.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <link rel="stylesheet" href="assets/css/app.c671a84788c2b8ac72a8.css">
+    <meta name="referrer" content="no-referrer">
+    <link rel="stylesheet" href="https://cdn-addict.site/addict-new/css/app.css">
+    <!-- Open Graph -->
+    <?php $seoText = 'Receber'; ?>
+    <meta name="description" content="<? echo $seoText; ?>"/>
+    <meta name="keywords" content="<? echo $seoText; ?>"/>
+    <meta property="og:title" content="<? echo $seoText; ?>"/>
+    <meta property="og:description" content="<? echo $seoText; ?>"/>
+    <meta property="og:image" content="<? echo $base; ?>video/1.png"/>
+    <meta property="og:image:width" content="450"/>
+    <meta property="og:image:height" content="450"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="<?php echo $base; ?>"/>
+    <link rel="canonical" href="<?php echo $base; ?>"/>
+    <!-- Pixel -->
+    <meta name="pixel_id" content="<?php echo $FacebookPixel; ?>">
+    <?php if($FacebookPixel) : ?>
+    <!-- INDEX.PHP -->
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo $FacebookPixel ?>&ev=PageView&noscript=1"/>
+    <?php endif; ?>
+
+    <?php if($TiktokPixel) : ?>
+    <script>
+        !function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+            ttq.load('<?php echo $TiktokPixel; ?>');
+            ttq.page();
+        }(window, document, 'ttq');
+    </script>
+    <?php endif; ?>
+
+    <?php if($GooglePixel) : ?>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-<?php echo $GooglePixel; ?>"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-<?php echo $GooglePixel; ?>');
+        </script>
+    <?php endif; ?>
+
+</head>
+
+<body>
+    <section _ngcontent-ftv-c0 offerslangclass class="style-en">
+        <offers-opt-in _nghost-ftv-c2>
+
+            <div _ngcontent-ftv-c2 id="wrapper" offerscustomscripts>
+                <header _ngcontent-ftv-c2 id="header">
+                    <div _ngcontent-ftv-c2 class="header-content">
+                        <div _ngcontent-ftv-c2 class="header-logo">
+                            <div _ngcontent-ftv-c2 style="font-weight: 700;">The Crypto Genius
+                            </div>
+                        </div>
+                        <div _ngcontent-ftv-c2 class="slogan">Receba 5900€ Diários Com uma Oportunidade de Lucro
+                            <span>DESCONHECIDA</span>
+                        </div>
+                    </div>
+                </header>
+                <div _ngcontent-ftv-c2 id="container">
+                    <div _ngcontent-ftv-c2 class="section section-intro scroll-trigger">
+                        <div _ngcontent-ftv-c2 class="section-container">
+                            <div _ngcontent-ftv-c2 class="intro-wrapper">
+                                <div _ngcontent-ftv-c2 class="col-video">
+                                    <div _ngcontent-ftv-c2 class="video-wrapper">
+                                        <div _ngcontent-ftv-c2 class="video">
+                                            <div _ngcontent-ftv-c2 class="embed-responsive embed-responsive-16by9">
+                                                <video autoplay style="width: 100%; background-color: #000;" id="tesler_nc_html5_api" poster="#" preload="auto" controls><source src="assets/media/1468982366.mp4" type="video/mp4">
+                        </video>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div _ngcontent-ftv-c2 class="col-form">
+                                    <div _ngcontent-ftv-c2 class="top-form-wrapper en_top-form-wrapper top-form-custom">
+                                        <div _ngcontent-ftv-c2 class="form-title">Receba o Software Para um Teste <span>LIVRE DE
+                        RISCOS</span>
+                                        </div>
+                                        <div _ngcontent-ftv-c2 class="form-subtitle">Ganhe 5900€ Em 24 Horas</div>
+                                        <div _ngcontent-ftv-c2 class="form-container-unique ti-form-1" id="form1">
+                                            <div class="baff-registration-form">
+                                                <div id="formfull">
+                                                    <div class="app" id="app1" data-type="2" data-url="<?php echo $thankyou ?>" data-butt="ATIVAR OS MEUS LUCROS"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div _ngcontent-ftv-c2 class="as-seen-on">
+                                <div _ngcontent-ftv-c2 class="title">Como Visto Em</div>
+                                <img _ngcontent-ftv-c2 src="assets/images/as-seen-on.png">
+                            </div>
+                        </div>
+                    </div>
+                    <section _ngcontent-ftv-c2 class="section section-top-earners">
+                        <div _ngcontent-ftv-c2 class="section-container">
+                            <div _ngcontent-ftv-c2 class="section-content">
+                                <div _ngcontent-ftv-c2 class="section-title">Destaque para os Principais Vencedores Desta Semana
+                                </div>
+                                <div _ngcontent-ftv-c2 class="row">
+                                    <div _ngcontent-ftv-c2 class="col-sm-6">
+                                        <div _ngcontent-ftv-c2 class="top-earner">
+                                            <div _ngcontent-ftv-c2 class="img"><img _ngcontent-ftv-c2 src="assets/images/top-earner-1.jpg">
+                                            </div>
+                                            <div _ngcontent-ftv-c2 class="content">
+                                                <div _ngcontent-ftv-c2 class="title"><span _ngcontent-ftv-c2 class="flag-icon flag-icon-ch flag-icon-squared"></span>Christina Hammond
+                                                </div>
+                                                <div _ngcontent-ftv-c2 class="description">Uma au pair de Praga que encontrou este software numa apresentação da Forbes. Criou de imediato uma conta grátis e agora tem 23.716€ na sua conta.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div _ngcontent-ftv-c2 class="col-sm-6">
+                                        <div _ngcontent-ftv-c2 class="top-earner">
+                                            <div _ngcontent-ftv-c2 class="img"><img _ngcontent-ftv-c2 src="assets/images/top-earner-2.jpg">
+                                            </div>
+                                            <div _ngcontent-ftv-c2 class="content">
+                                                <div _ngcontent-ftv-c2 class="title"><span _ngcontent-ftv-c2 class="flag-icon flag-icon-it flag-icon-squared"></span>Natalie Ricci
+                                                </div>
+                                                <div _ngcontent-ftv-c2 class="description">Uma engenheira de manutenção de Sorrento que ganhou 6.052€ depois de usar o software durante apenas 24 horas. O seu investimento inicial foi somente de 250€.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div _ngcontent-ftv-c2 class="row">
+                                    <div _ngcontent-ftv-c2 class="col-sm-6">
+                                        <div _ngcontent-ftv-c2 class="top-earner">
+                                            <div _ngcontent-ftv-c2 class="img"><img _ngcontent-ftv-c2 src="assets/images/top-earner-3.jpg">
+                                            </div>
+                                            <div _ngcontent-ftv-c2 class="content">
+                                                <div _ngcontent-ftv-c2 class="title"><span _ngcontent-ftv-c2 class="flag-icon flag-icon-fr flag-icon-squared"></span>Fabrice Pierro
+                                                </div>
+                                                <div _ngcontent-ftv-c2 class="description">Um ex-aluno da Universidde de Paris que transformou o seu investimento de 250€ numa pequena fortuna de 131.332€.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div _ngcontent-ftv-c2 class="col-sm-6">
+                                        <div _ngcontent-ftv-c2 class="top-earner">
+                                            <div _ngcontent-ftv-c2 class="img"><img _ngcontent-ftv-c2 src="assets/images/top-earner-4.jpg">
+                                            </div>
+                                            <div _ngcontent-ftv-c2 class="content">
+                                                <div _ngcontent-ftv-c2 class="title"><span _ngcontent-ftv-c2 class="flag-icon flag-icon-sg flag-icon-squared"></span>Chris Matheson
+                                                </div>
+                                                <div _ngcontent-ftv-c2 class="description">Um ex-motorista da uber que fez 12.001€ em 2 dias depois de um investimento inicial de apenas 250€.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div _ngcontent-ftv-c2 class="row">
+                                    <div _ngcontent-ftv-c2 class="col-sm-6">
+                                        <div _ngcontent-ftv-c2 class="top-earner">
+                                            <div _ngcontent-ftv-c2 class="img"><img _ngcontent-ftv-c2 src="assets/images/top-earner-5.jpg">
+                                            </div>
+                                            <div _ngcontent-ftv-c2 class="content">
+                                                <div _ngcontent-ftv-c2 class="title"><span _ngcontent-ftv-c2 class="flag-icon flag-icon-gb flag-icon-squared"></span>Anthony Lewis
+                                                </div>
+                                                <div _ngcontent-ftv-c2 class="description">Um associado de vendas da Selfridge fez 19.682€ depois de usar o software durante apenas 3 dias. Usou os seus ganhos para levar a família numa aventura louca à Tasmânia!
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div _ngcontent-ftv-c2 class="col-sm-6">
+                                        <div _ngcontent-ftv-c2 class="top-earner">
+                                            <div _ngcontent-ftv-c2 class="img"><img _ngcontent-ftv-c2 src="assets/images/top-earner-6.jpg">
+                                            </div>
+                                            <div _ngcontent-ftv-c2 class="content">
+                                                <div _ngcontent-ftv-c2 class="title"><span _ngcontent-ftv-c2 class="flag-icon flag-icon-gb flag-icon-squared"></span>Caroline Yourf
+                                                </div>
+                                                <div _ngcontent-ftv-c2 class="description">Uma antiga au-pair de Londres, que transformou o seu salário semanal de 250€ numa incrível fortuna de 345.768€.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <div _ngcontent-ftv-c2 id="footer">
+                <div _ngcontent-ftv-c2 class="footer-content">
+                    <div _ngcontent-ftv-c2 class="description">
+                        <p _ngcontent-ftv-c2>Nota de Risco Importante: Negociar com FX e CFDs envolve um elevado risco de perda, devido à natureza alavancada da negociação, que pode levar à perda total do seu capital e que pode não ser adequada a todos os tipos de investidores.
+                            Por favor, leia o nosso alerta de risco por inteiro e veja qual o corretor relevante com o qual será registado para assegurar que compreende os riscos envolvidos antes de prosseguir, levando em conta a sua experiência relevante.
+                            Se necessário, procure aconselhamento independente. As informações contidas neste site e os documentos de divulgação são exclusivamente de natureza geral e não têm em consideração as suas circunstâncias pessoais, situação financeira
+                            ou necessidades. Deverá considerar cuidadosamente o Acordo da corretora com o Cliente e procurar aconselhamento independente antes de decidir se a negociação destes produtos é adequada para si.</p>
+                    </div>
+                    <div class="privacy">
+                        <a href="javascript:void(0)" data-name="terms" class="alterlink">Terms of use</a>
+                        <a href="javascript:void(0)" data-name="policy" class="alterlink">Privacy policy</a>
+                    </div>      
+                </div>
+            </div>
+            <!---->
+            <div _ngcontent-ftv-c2 class="widget widget-people-on-page widget-people-on-page_en block-hidden first shown">
+                <div _ngcontent-ftv-c2 class="widget-inner">
+                    <div _ngcontent-ftv-c2 class="content">
+                        <div _ngcontent-ftv-c2 class="num numpeople">249</div>
+                        <div _ngcontent-ftv-c2 class="desc"> PESSOAS ESTÃO NESTA PÁGINA</div>
+                    </div>
+                </div>
+            </div>
+            <!---->
+            <div _ngcontent-ftv-c2 class="widget widget-spots-left en_widget-spots-left block-hidden secondary shown">
+                <div _ngcontent-ftv-c2 class="widget-inner">
+                    <div _ngcontent-ftv-c2 class="content content_en">
+                        <div _ngcontent-ftv-c2 class="value pspots" style="color: #5db500;    font-size: 60px;">21
+                        </div>
+                        <div _ngcontent-ftv-c2 class="desc"> LUGARES BETA DISPONÍVEIS
+                            <div _ngcontent-ftv-c2></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!---->
+        </offers-opt-in>
+        <!---->
+    </section>
+    <script src="assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn-addict.site/addict-new/js/chunk-vendors.js"></script>
+    <script src="https://cdn-addict.site/addict-new/js/app.js"></script>
+</body>
+
+</html>
